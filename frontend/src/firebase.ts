8da321812c92249829a4036c630a9d839b7528f0
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2025-11-02 16:22:23 +0530

    fix : Solved the proble of sign in with google it is because of double file "

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"; // Add this for authentication//
 // update packages  // updaet
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClG9EbU9n_3tyAOiV2MJADnp9YV5gUh2Q",
-  authDomain: "bombooworld-ba410.firebaseapp.com",
-  projectId: "bombooworld-ba410",
-  storageBucket: "bombooworld-ba410.firebasestorage.app",
-  messagingSenderId: "1055253396005",
-  appId: "1:1055253396005:web:b629f6b117a15ce437ddcc",
-  measurementId: "G-1342KP4ZW0"
+  apiKey: "AIzaSyDkTZRwtDK_P-MCSGYh3kkMe9v-0aSOziU",
+  authDomain: "bombooworld-ac27a.firebaseapp.com",
+  projectId: "bombooworld-ac27a",
+  storageBucket: "bombooworld-ac27a.firebasestorage.app",
+  messagingSenderId: "80642417540",
+  appId: "1:80642417540:web:631cad0696eba28422b3d1",
+  measurementId: "G-DL1KJHXP5R"
 };
 
 // Initialize Firebase  
